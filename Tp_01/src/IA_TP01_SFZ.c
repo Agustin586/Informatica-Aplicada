3390fc265272610f4ff9e6c52b4913a2b881ec32
Agustin586 <marcelo123z.com@gmail.com>
2023-04-28 21:57:38 -0300

    Inicio del proyecto

---
+++ Tp_01/src/IA_TP01_SFZ.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : IA_TP01_SFZ.c
+ Author      : Zuliani, Agustin
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts(""); /* prints  */
+	return EXIT_SUCCESS;
+}
