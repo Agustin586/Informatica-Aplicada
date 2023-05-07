487fae5cfe51ea49614164279f1fb4d63f13e947
Agustin586 <marcelo123z.com@gmail.com>
2023-05-07 11:17:52 -0300

    .

--- Tp_01/src/IA_TP01_SFZ.c
+++ Tp_01/src/IA_TP01_SFZ.c
@@ -83,6 +83,7 @@ void Init_estados(int *est_archivos,int *est_linea);
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 volatile uint8_t fin_programa = 0;	//No debe optimizarse
 =======
 
@@ -92,6 +93,9 @@ uint8_t fin_programa = 0;
 
 uint8_t fin_programa = 0;
 >>>>>>> parent of 9336bb9 (Programa finalizado)
+=======
+volatile uint8_t fin_programa = 0;
+>>>>>>> parent of b2200d5 (.)
 
 int main(void){
 	srand(time(NULL));
